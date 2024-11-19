9051318d7071399ceeb10138d3fc5a4d1f5f6a27
Noam Schuck <noam.schuck@gmail.com>
2024-11-18 21:08:37 -0500

    finished pset4

--- ps4/main.m
+++ ps4/main.m
@@ -2,6 +2,8 @@ clc;
 clear;
 close all;
 
+format long;
+
 %% Question 2
 fc = 10;
 kf = 3;
@@ -44,4 +46,30 @@ for i = 1:3
     title("Signal Modulated with Amplitude " + amps(i));
     xlabel("time (s)");
     ylabel("amplitude (V)");
+end
+
+%% Question 4
+
+syms r
+
+eq1 = 10e-4 == exp(-r^2/2);
+
+sol = solve([eq1], [r]);
+
+threshold = double(sol(1));
+n = 1e7;
+sigma = [10; 10; 10] .^ [0; -1/10; 1/10]; 
+p = exp(-threshold^2*ones(size(sigma))/2./sigma);
+R =  sqrt(sigma/2) .* abs(randn(3,n) + 1j*randn(3,n));
+
+percent = sum(R > threshold, 2)/length(R);
+
+for i = 1:3
+    figure; 
+    plot(R(i, :));
+    yline(threshold, "-r", "Threshold", 'LineWidth', 3);
+    title("R with sigma^2 = " + sigma(i));
+
+    fprintf("Fraction above threshold for sigma^2 = %f: %f < %f\n", sigma(i), percent(i), p(i));
+
 end
